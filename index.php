db9ea4a926b885c2edc4ac10e02421b595cadcbc
abdalkrem <abdalkrem.alkhmes@stu.ibu.edu.ba>
2021-04-01 20:58:10 +0200

    Added a connection to the database.

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once 'vendor/autoload.php';
+
+require_once dirname(__FILE__).'/dao/BaseDao.class.php';
+
+Flight::register('dao', 'BaseDao', ['users']);
 
 Flight::route('/', function() {
     echo 'Welcome to my application.';
 });
 
 Flight::route('/users', function() {
-    echo 'This page display users!';
+    $users = Flight::dao()->get_all();
+    Flight::json($users);
 });
 
 Flight::route('/users/@id', function($id) {
